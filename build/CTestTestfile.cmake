ddacaf0f534e55f14214117d0f47e38d394195e6
mertgungor <62485435+mertgungor@users.noreply.github.com>
2021-08-27 14:37:38 +0300

    everybody should run catkin_make to produce these files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mert/QTProjects/Instrument/src
-# Build directory: /home/mert/QTProjects/Instrument/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basict")
--- build/basict/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/mert/QTProjects/Instrument/src/basict/include".split(';') if "/home/mert/QTProjects/Instrument/src/basict/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;stdmsgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lbasict".split(';') if "-lbasict" != "" else []
-PROJECT_NAME = "basict"
-PROJECT_SPACE_DIR = "/home/mert/QTProjects/Instrument/devel"
-PROJECT_VERSION = "0.0.0"
